81da3bbf42c81c46cc52ea8a080cab46a081fa9e
Inuk Ryjewsky <ryjewski@gmail.com>
2020-03-24 10:35:43 +0100

    Text box + button

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button class="btn">{{ text }}Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button"
+};
+</script>
+
+<style></style>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="holder">
+    <h2 class="holder__hedding">
+      {{ hedding }}
+    </h2>
+    <p class="holder__content">
+      {{ content1 }}
+    </p>
+    <p class="holder__content">
+      {{ content2 }}
+    </p>
+    <p class="holder__content">
+      {{ content3 }}
+    </p>
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from "../components/Button";
+
+export default {
+  name: "TextBox",
+  components: {
+    Button
+  }
+};
+</script>
+
+<style></style>
